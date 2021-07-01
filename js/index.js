871536e4a71611e0acf754d5c70b61b766368d01
Francisco Aires <xicojr@gmail.com>
2021-07-01 15:28:53 -0300

    incluindo javascript

---
+++ js/index.js
@@ -0,0 +1,38 @@
+function validar() {
+        let nome = formulario.nome;
+        let email = formulario.email;
+        let telefone = formulario.telefone;
+        let mensagem = formulario.mensagem;
+
+        if (nome.value == "") {
+            alert("Favor informar seu nome");
+            nome.focus();
+            return;        
+        }
+        if (email.value == "") {
+            alert("Favor informar seu e-mail");
+            email.focus();
+            return;        
+        }
+        if (telefone.value == "") {
+            alert("Favor informar seu telefone");
+            telefone.focus();
+            return;        
+        }
+        if (mensagem.value == "") {
+            alert("Favor informar a mensagem");
+            mensagem.focus();
+            return;        
+        }
+    }   
+    
+    function formatar(mascara, documento){
+        var i = documento.value.length;
+        var saida = mascara.substring(0,1);
+        var texto = mascara.substring(i)
+        
+        if (texto.substring(0,1) != saida){
+                  documento.value += texto.substring(0,1);
+        }
+        
+      }
